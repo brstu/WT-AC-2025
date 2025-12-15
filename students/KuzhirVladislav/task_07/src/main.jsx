274e4c80e17d11148915790660d0438dfe59e8d9
XD-cods <vkuzir7@gmail.com>
2025-12-15 21:46:45 +0300

    fix

--- students/KuzhirVladislav/task_07/src/main.jsx
+++ students/KuzhirVladislav/task_07/src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from '../../task_07/src/store/store.js';
-import App from './App.jsx';
-import './assets/style.css';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
+import App from "./App.jsx";
+import "./assets/style.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <App />
