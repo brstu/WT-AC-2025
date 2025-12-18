c8fb3b91c91de536872c9a9715c2f6786b37e891
Andrei21005 <evkovich.andrej@mail.ru>
2025-12-18 12:29:20 +0300

    add [task_07]

---
+++ students/EvkovichAndrey/task_07/src/src/providers/QueryProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
+
+export function QueryProvider({ children }: { children: ReactNode }) {
+  // useState — чтобы QueryClient создавался только на клиенте
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 5, // 5 минут
+            retry: 1,
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  );
+}
