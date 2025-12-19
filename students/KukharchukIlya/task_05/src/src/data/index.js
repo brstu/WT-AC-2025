0bc4682e6caa0029058ad553542be15ac4818b2f
Ilya Kukharchuk <bartash12345@gmail.com>
2025-12-19 18:18:57 +0300

    add task05

---
+++ students/KukharchukIlya/task_05/src/src/data/index.js
@@ -0,0 +1,12 @@
+// In-memory data storage
+let books = [];
+let reviews = [];
+let currentBookId = 1;
+let currentReviewId = 1;
+
+module.exports = {
+  books,
+  reviews,
+  currentBookId,
+  currentReviewId
+};
