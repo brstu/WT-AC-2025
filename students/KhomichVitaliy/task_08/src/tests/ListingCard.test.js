c8c17804a4491cf5be131b1d342e283100e1131b
VitalyaNB <vitalya.khomich15@gmail.com>
2025-12-23 09:22:52 +0300

    add task08

--- students/KhomichVitaliy/task_08/src/tests/ListingCard.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import ListingCard from "../src/ListingCard";
-
-test("renders listing title", () => {
-  render(<ListingCard title="Cozy Apartment" />);
-  expect(screen.getByText(/Cozy Apartment/i)).toBeInTheDocument();
-});
--- students/KhomichVitaliy/task_08/src/tests/rental.e2e.js
+++
@@ -1,6 +0,0 @@
-describe("Rental Catalog", () => {
-  it("loads home page", () => {
-    cy.visit("http://localhost:3000");
-    cy.contains("Available Rentals");
-  });
-});
