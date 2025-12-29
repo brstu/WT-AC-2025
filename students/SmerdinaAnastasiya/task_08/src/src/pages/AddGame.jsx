e02479310423c36a18155ba67497bb794a2f01d0
Anastasia Smerdina <asmerdina@mail.ru>
2025-12-29 23:34:16 +0300

    [AutoLabSuite] task_08

---
+++ students/SmerdinaAnastasiya/task_08/src/src/pages/AddGame.jsx
@@ -0,0 +1,13 @@
+
+import GameForm from '../components/forms/GameForm'
+import './FormPage.css'
+
+const AddGame = () => {
+  return (
+    <div className="form-page">
+      <GameForm />
+    </div>
+  )
+}
+
+export default AddGame
